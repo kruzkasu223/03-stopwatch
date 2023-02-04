33cb9b44a1c1b1dd88b22707aa7d56f30e61269b
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-04 20:37:56 +0530

    feat: done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 03/27-stopwatch
 
-This project is all about building a custom stopwatch from the ground up.
+This project is all about building a custom stopwatch from the ground up. Will only be creating functionality of stopwatch, not focusing on design.
 
 I will use [React](https://reactjs.org/) with [TypeScript](https://www.typescriptlang.org/) and [vite bundler](https://vitejs.dev/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
+import { useEffect, useState } from "react"
 import "./App.css"
 
+const getFormatted = (number: number) => `${number < 10 ? 0 : ""}${number}`
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [time, setTime] = useState(0)
+  const [laps, setLaps] = useState<string[]>([])
+
+  const hours = getFormatted(Math.floor((time / (1000 * 60 * 60)) % 24))
+  const minutes = getFormatted(Math.floor((time / (1000 * 60)) % 60))
+  const seconds = getFormatted(Math.floor((time / 1000) % 60))
+  const milliseconds = getFormatted(Math.floor((time % 1000) / 10))
+
+  const handleAddLaps = () =>
+    setLaps((laps) => [
+      `${hours}:${minutes}:${seconds}.${milliseconds}`,
+      ...laps,
+    ])
+
+  const handleResetLaps = () => setLaps([])
+
+  const handleStart = () => setIsActive((isOn) => !isOn)
+
+  const handleReset = () => setTime(0)
+
+  useEffect(() => {
+    let handleInterval: ReturnType<typeof setInterval> | null = null
+
+    if (isActive) {
+      handleInterval = setInterval(() => {
+        setTime((t) => t + 51)
+      }, 51)
+      // ^^ added 51ms ddelay because browser won't be able to handle 1ms delay
+      // or even if it does, event loop will not be able to keep up
+      // with such short delay and will record inconsistencies
+    } else {
+      handleInterval && clearInterval(handleInterval)
+    }
+
+    return () => {
+      handleInterval && clearInterval(handleInterval)
+    }
+  }, [isActive])
 
   return (
     <div className="App">
       <h1>03/27 - Stopwatch</h1>
+      <div className="main">
+        <div>
+          <div className="time_container">
+            <span className="time">
+              <span className="hours">{hours}</span>
+              <span className="minutes">:{minutes}</span>
+              <span className="seconds">:{seconds}</span>
+              <span className="milliseconds">.{milliseconds}</span>
+            </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={handleStart}>{isActive ? "Stop" : "Start"}</button>
+          <button onClick={handleReset}>Reset</button>
+          <button onClick={handleAddLaps}>Add Lap</button>
+          <button onClick={handleResetLaps}>Reset Laps</button>
+        </div>
+        <div className="laps">
+          {laps?.map((lap, i) => (
+            <span key={lap + i} className="lap">
+              {lap}
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
